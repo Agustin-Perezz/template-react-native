6daba880f44ff5277bd7a006e82dbea1ddf70f37
Agustin-Perezz <agustinnicolas806@gmail.com>
2025-12-27 08:55:25 -0300

    Configure root layout with protected routes and authentication

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,17 @@
 import { Stack } from 'expo-router';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import '../global.css';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Protected guard={false}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack.Protected>
+
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
